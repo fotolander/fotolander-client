9d2b1f51433c40bd4d70d6f35fd5bbae1e80dff6
Gabi <gbrielamegiel@gmail.com>
2021-07-06 14:18:10 +0200

    working map with places - agm map

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 // import {MouseEvent} from "@agm/core";
-import {MapService} from "../../service/map.service";
+// import {MapService} from "../../service/map.service";
+import {Place} from "../../model/place.model";
+import {PlaceService} from "../../service/place.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-map',
@@ -9,64 +12,100 @@ import {MapService} from "../../service/map.service";
 })
 export class MapComponent implements OnInit {
 
+  title = 'Get inspired';
+  lat = 52.068487910382366;
+  lng = 19.47964484087672;
 
+  public places: Place[] = [];
 
-  // latitude = 52.068487910382366;
-  // longitude = 19.47964484087672;
-  // zoom: number = 7 ;
+  constructor(
+    private placesService: PlaceService,
+    private router: Router) {
+    placesService.showPlaces().subscribe(
+      (places: Place[]) => {
+        this.places = places;
+      });
+  }
 
-  // clickedMarker(label: string, index: number) {
-  //   console.log(`clicked the marker: ${label || index}`)
-  // }
+  ngOnInit(): void {
+  }
 
-  // mapClicked($event: MouseEvent) {
-  //   this.markers.push({
-  //     lat: $event.coords.lat,
-  //     lng: $event.coords.lng,
-  //     draggable: true
-  //   });
-  // }
-
-  // markerDragEnd(m: marker, $event: MouseEvent) {
-  //   console.log('dragEnd', m, $event);
-  // }
-
-  // markers: marker[] = [
-  //   {
-  //     lat: 51.673858,
-  //     lng: 19.815982,
-  //     label: 'A',
-  //     draggable: true
-  //   },
-  //   {
-  //     lat: 51.373858,
-  //     lng: 18.215982,
-  //     label: 'B',
-  //     draggable: false
-  //   },
-  //   {
-  //     lat: 51.723858,
-  //     lng: 21.895982,
-  //     label: 'C',
-  //     draggable: true
-  //   }
-  // ]
-
-  // onChoseLocation(event){
-  //   this.latitude = event.coords.lat;
-  //   this.longitude = event.coords.lng;
-  // }
-
-  constructor(private map: MapService) { }
-
-  ngOnInit() {
-    this.map.buildMap()
+  onGoToThisPlace(name: string) {
+    this.router.navigate(['/','places','all',name]);
   }
 }
 
-// interface marker {
-//   lat: number;
-//   lng: number;
-//   label?: string;
-//   draggable: boolean;
+//do uruchomienia mpy z agm
+// export class MapComponent implements OnInit {
+//
+//   title = 'Get inspired';
+//   lat = 52.068487910382366;
+//   lng = 19.47964484087672;
+//
+//   public places: Place[] = [];
+//
+//   constructor(
+//     private placesService: PlaceService,
+//     private router: Router) {
+//     placesService.showPlaces().subscribe(
+//       (places: Place[]) => {
+//         this.places = places;
+//       });
+//   }
+//
+//   ngOnInit(): void {
+//   }
+//
+//   onGoToThisPlace(name: string) {
+//     this.router.navigate(['/','places','all',name]);
+//   }
+// }
+
+
+
+
+// latitude = 52.068487910382366;
+// longitude = 19.47964484087672;
+// zoom: number = 7 ;
+
+// clickedMarker(label: string, index: number) {
+//   console.log(`clicked the marker: ${label || index}`)
+// }
+
+// mapClicked($event: MouseEvent) {
+//   this.markers.push({
+//     lat: $event.coords.lat,
+//     lng: $event.coords.lng,
+//     draggable: true
+//   });
+// }
+
+// markerDragEnd(m: marker, $event: MouseEvent) {
+//   console.log('dragEnd', m, $event);
+// }
+
+// markers: marker[] = [
+//   {
+//     lat: 51.673858,
+//     lng: 19.815982,
+//     label: 'A',
+//     draggable: true
+//   },
+//   {
+//     lat: 51.373858,
+//     lng: 18.215982,
+//     label: 'B',
+//     draggable: false
+//   },
+//   {
+//     lat: 51.723858,
+//     lng: 21.895982,
+//     label: 'C',
+//     draggable: true
+//   }
+// ]
+
+// onChoseLocation(event){
+//   this.latitude = event.coords.lat;
+//   this.longitude = event.coords.lng;
 // }
--- src/app/service/map.service.ts
+++ src/app/service/map.service.ts
@@ -1,36 +1,36 @@
-import { Injectable } from '@angular/core';
-import * as mapboxgl from 'mapbox-gl';
-import { environment } from "src/environments/environment";
-import {map} from "rxjs/operators";
-@Injectable({
-  providedIn: 'root'
-})
-export class MapService {
-  map: mapboxgl.Map;
-  style = 'mapbox://styles/zolleander/ckqkp1l434bfm17pfy41pfmiy';
-  lat = 52.068487910382366;
-  lng = 19.47964484087672;
-  zoom = 6
-  constructor() {
-    mapboxgl.accessToken = environment.mapbox.accessToken;
-  }
-  buildMap() {
-    this.map = new mapboxgl.Map({
-      container: 'map',
-      style: this.style,
-      zoom: this.zoom,
-      center: [this.lng, this.lat]
-    })
-    this.map.addControl(new mapboxgl.NavigationControl());
-    this.newMarker(18.643798828125, 54.351754357424696)
-  }
-
-  newMarker(lng: number, lat: number) {
-    const marker = new mapboxgl.Marker({
-      draggable:false
-    })
-      .setLngLat([lng,lat])
-      .addTo(this.map);
-  }
-
-}
+// import { Injectable } from '@angular/core';
+// import * as mapboxgl from 'mapbox-gl';
+// import { environment } from "src/environments/environment";
+// import {map} from "rxjs/operators";
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class MapService {
+//   map: mapboxgl.Map;
+//   style = 'mapbox://styles/zolleander/ckqkp1l434bfm17pfy41pfmiy';
+//   lat = 52.068487910382366;
+//   lng = 19.47964484087672;
+//   zoom = 6
+//   constructor() {
+//     mapboxgl.accessToken = environment.mapbox.accessToken;
+//   }
+//   buildMap() {
+//     this.map = new mapboxgl.Map({
+//       container: 'map',
+//       style: this.style,
+//       zoom: this.zoom,
+//       center: [this.lng, this.lat]
+//     })
+//     this.map.addControl(new mapboxgl.NavigationControl());
+//     this.newMarker(18.643798828125, 54.351754357424696)
+//   }
+//
+//   newMarker(lng: number, lat: number) {
+//     const marker = new mapboxgl.Marker({
+//       draggable:false
+//     })
+//       .setLngLat([lng,lat])
+//       .addTo(this.map);
+//   }
+//
+// }
